70b88277ddca7f1a6dbbd43518c7284397f25d9c
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-07 04:35:20 +0300

    Add static settings

--- ecomsite/urls.py
+++ ecomsite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
 
 from shop.views import ProductsListView
 
@@ -23,4 +25,4 @@ from shop.views import ProductsListView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', ProductsListView.as_view(),name='list_products'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

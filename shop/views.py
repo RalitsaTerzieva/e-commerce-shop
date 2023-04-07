d3f523bc2bd3c4a88bf42e6d40d00e3f2711c0df
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-08 01:31:06 +0300

    Add pagination on index page

--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from .models import Products
 from django.views.generic.list import ListView
 
+
 class ProductsListView(ListView):
     model = Products
     template_name = 'shop/index.html'
+    paginate_by = 4
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        item_name = self.request.GET.get('item_name')
+        if item_name != '' and item_name is not None:
+            queryset = queryset.filter(title__icontains=item_name)
+            
+        return queryset

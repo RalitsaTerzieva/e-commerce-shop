df70a0ead22ca213d131dcc658035c94b306a59e
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-11 13:31:00 +0300

    Customize Django admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Products, Order
 
-# Register your models here.
-admin.site.register(Products)
+admin.site.site_header = 'E-commerce admin pannel'
+admin.site.site_title = 'E-commerce site'
+admin.site.index_title = 'E-commerce administration'
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'category', 'description', 'image')
+    search_fields = ('title', 'category')
+    actions = ('change_category_to_default',)
+    
+    def change_category_to_default(self,request, queryset):
+        queryset.update(category='default')
+
+
+admin.site.register(Products, ProductAdmin)
 admin.site.register(Order)
